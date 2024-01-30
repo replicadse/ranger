012976abdbfffce04211e2ab760c24fdd955aa74
Alexander Weber <alexanderh.weber@outlook.com>
2024-01-29 19:01:14 -0800

    init

---
+++ src/error.rs
@@ -0,0 +1,21 @@
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    // #[error("generic {0}")]
+    // Generic(String),
+    #[error("many: {0:?}")]
+    Many(Vec<anyhow::Error>),
+    #[error("experimental command: {0}")]
+    ExperimentalCommand(String),
+    #[error("argument {0}")]
+    Argument(String),
+    #[error("child process {0}")]
+    ChildProcess(String),
+    #[error("node recursion")]
+    NodeRecursion,
+    #[error("unknown command")]
+    UnknownCommand,
+    #[error("version compatibility {0}")]
+    VersionCompatibility(String),
+    #[error("not found {0}")]
+    NotFound(String),
+}
